3a5a377d2a4989c7ab83117961ac8ff0696d873b
Zahra Mammadli <zahra.mrj@gmail.com>
2021-12-11 19:58:51 +0100

    Move functions from server.js to utils, turned to class and modified to loop on the response for create queries

--- utils/functions.js
+++ utils/functions.js
@@ -1,20 +1,31 @@
+const cTable = require("console.table");
 
+// This method runs queries and generate table based on the resul
 
+class sqlQueries {
+  constructor(query, values) {
+    this.query = query;
+    this.values = values;
+  }
 
-//  Function to run sql scripts returning data
-const runSql = async (script) => {
-    connection.query(script, (err, data) => {
+  //  Function to run sql scripts returning data
+  generateTable(main) {
+    connection.query(this.query, this.values, (err, data) => {
       if (err) throw err;
       console.table(data);
       main();
     });
-  };
-  //  Function to run sql scripts to add/modify data
-  modifySql = (script, responce) => {
-    connection.query(script, responce, (err, data) => {
+  }
+  getQueryNoRepeats(nextStep, parameterToPassToNextStep) {
+    connection.query(this.query, this.values, function (err, res) {
       if (err) throw err;
-      console.table(data);
-      main();
+      let titleArr = [];
+      for (let i = 0; i < res.length; i++) {
+        if (!titleArr.includes(res[i].title)) {
+          titleArr.push(res[i].title);
+        }
+      }
+      nextStep(titleArr, parameterToPassToNextStep);
     });
-  };
-  
+  }
+}
